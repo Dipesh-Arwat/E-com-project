6e13b19934bbfbb61eae61744a810d8a38300e3b
Dipesh Arwat <dipesharwat80@gmail.com>
2024-09-29 10:28:24 +0530

    change link

--- e-commerce/src/Components/Popular/Popular.jsx
+++ e-commerce/src/Components/Popular/Popular.jsx
@@ -10,7 +10,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('https://e-com-project-backend.onrender.com/popularinwomen')
+    fetch('http://localhost:4000/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data))
   },[])
